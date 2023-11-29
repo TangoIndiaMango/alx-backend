85fd41128213be2a89e33cae67294708fb927617
Timilehin Aliyu <89034378+TangoIndiaMango@users.noreply.github.com>
2023-11-29 13:25:11 -0800

    Update 8-job.js

--- 0x03-queuing_system_in_js/8-job.js
+++ 0x03-queuing_system_in_js/8-job.js
@@ -2,8 +2,8 @@ function createPushNotificationsJobs(jobs, queue) {
   if (!Array.isArray(jobs)) {
     throw Error('Jobs is not an array');
   }
-
-  jobs.forEach((myJob) => {
+// because it's needs to yield a result
+  for (const myJob of jobs) {
     let job = queue.create('push_notification_code_3', myJob);
 
     job.on('complete', function() {
@@ -13,10 +13,11 @@ function createPushNotificationsJobs(jobs, queue) {
     }).on('progress', function(progress, data) {
       console.log(`Notification job ${job.id} ${progress}% complete`);
     });
+
     job.save((error) => {
       if (!error) console.log(`Notification job created: ${job.id}`);
     });
-  });
+  }
 }
 
 module.exports = createPushNotificationsJobs;
